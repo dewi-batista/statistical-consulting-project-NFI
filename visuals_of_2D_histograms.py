b1d1856215b8c09dd851fad231a58907bcc56121
dewibatista <62663654+dewi-batista@users.noreply.github.com>
2025-03-30 23:16:33 +0200

    some changes

--- visuals_of_2D_histograms.py
+++ visuals_of_2D_histograms.py
@@ -7,11 +7,11 @@ data = pd.read_csv('data/preproc_mixtures.csv')
 
 # all six fluid combinations present in mixtures.csv
 fluid_combinations = [
-    # ('Semen.fertile', 'Vaginal.mucosa'),
-    # ('Saliva', 'Vaginal.mucosa'),
+    ('Semen.fertile', 'Vaginal.mucosa'),
+    ('Saliva', 'Vaginal.mucosa'),
     ('Blood', 'Nasal.mucosa'),
     ('Nasal.mucosa', 'Saliva'),
-    # ('Blood', 'Vaginal.mucosa'),
+    ('Blood', 'Vaginal.mucosa'),
     ('Blood', 'Menstrual.secretion')
 ]
 
@@ -19,17 +19,16 @@ fluid_combinations = [
 marker_start_index = list(data.columns).index("HBB")
 markers = data.columns[marker_start_index:]
 for col in markers:
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))  # Adjusted to 1 by 3
-    # fig.suptitle(f'Distributions of {col} conditioned on fluid combinations', fontsize=16, fontweight='bold')
-    
+    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+
     for i, (fluid_1, fluid_2) in enumerate(fluid_combinations):
-        ax = axes[i]  # Since it's a 1D array now
+        row, col_idx = divmod(i, 3)
+        ax = axes[row][col_idx]
         restricted_data = data[(data[fluid_1] == 1) & (data[fluid_2] == 1)]
         sns.histplot(restricted_data[col].dropna(), bins=30, kde=False, ax=ax)
         ax.set_title(f'{fluid_1} & {fluid_2}', fontweight='bold')
         ax.set_xlabel('Value')
         ax.set_ylabel('Freq.')
-    plt.tight_layout()
-    # plt.tight_layout(rect=[0, 0, 1, 0.95])
-    plt.savefig(f"figures/2D_hists/distributions_of_{col}.pdf")
 
+    plt.tight_layout()
+    plt.savefig(f"figures/2D_histograms/distributions_of_{col}.pdf")
